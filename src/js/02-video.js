f5b9c2f26642e5385e394bfe554bffa78b9774c4
StanyslavCherniakov <stas.chernykov@gmail.com>
2022-09-25 20:48:38 +0300

    task-02 - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import vimeoPlayer from '@vimeo/player';
+import throttle from 'throttleit';
+// import vimeo from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
+const player = new vimeoPlayer(iframe);
 
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+player.on('timeupdate', throttle(playedTime, 1000));
+
+function playedTime(data) {
+  localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+}
