3790aa362e3fe211a1ba8559c9432fa91f9556fc
StanyslavCherniakov <stas.chernykov@gmail.com>
2022-09-27 17:01:03 +0300

    Fixed task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,10 +4,17 @@ import throttle from 'throttleit';
 const iframe = document.querySelector('#vimeo-player');
 const player = new vimeoPlayer(iframe);
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+setCurrentTime();
 
 player.on('timeupdate', throttle(playedTime, 1000));
 
 function playedTime(data) {
   localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+}
+
+function setCurrentTime() {
+  const savedTimeToPlay = localStorage.getItem('videoplayer-current-time');
+  if (savedTimeToPlay) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+  }
 }
